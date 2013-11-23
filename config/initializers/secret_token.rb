8e7de0f28f3f476baf6204c62bd6d0adc3f60c98
jason <lizengzhong@gmail.com>
2013-11-23 00:31:37 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeSite::Application.config.secret_token = 'acfc0ff98d29c2fcf0963f64d6b7a01b53589b75dfdfec7fc3041682d1377fd582048a7517d63ebc8e26e2b175044b6b6f54260299f826a8a1703774ed5b4a61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HomeSite::Application.config.session_store :cookie_store, key: '_HomeSite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HomeSite::Application.config.session_store :active_record_store
